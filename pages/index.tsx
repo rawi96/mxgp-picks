2fce44818613e017f150049b1572fd33324b86b0
Raphi <raphael@smartive.ch>
2022-10-10 09:02:06 +0200

    link workflow

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world ASDFASDF!</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello world TEST LINT!</h1>;
 }

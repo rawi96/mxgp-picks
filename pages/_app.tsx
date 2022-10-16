b2982e5b3e348211b4f99c947e4de21d726ee836
Raphi <raphael@smartive.ch>
2022-10-16 13:43:50 +0200

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,6 +21,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="icon" href="/icons/favicon.ico" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <SessionProvider session={pageProps.session}>

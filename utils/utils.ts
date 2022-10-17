a091194693bb7a1f6daf3a2eadf791f93950a21b
Raphi <raphael@smartive.ch>
2022-10-17 13:18:53 +0200

    sign-up

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,5 +1,4 @@
-export const REGEX_EMAIL =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const REGEX_EMAIL = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
 // Password must contain minimum 6 and contain at least one numeric digit, one uppercase and one lowercase letter!
 export const REGEX_PASSWORD = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;

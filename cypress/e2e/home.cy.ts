bd6d7bdc17f33baf53dd96b011c7c7f4be1332a0
Raphi <raphael@smartive.ch>
2022-11-07 17:44:17 +0100

    cypress pipeline

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,11 +1,19 @@
 import { LAYOUT_SELECTOR } from '../selectors';
 
-describe('/home opens properly', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
+describe(
+  '/home opens properly',
+  {
+    env: {
+      baseUrl: 'http://localhost:3000/',
+    },
+  },
+  () => {
+    beforeEach(() => {
+      cy.visit(`${Cypress.env('baseUrl')}`);
+    });
 
-  it('displays layout', () => {
-    cy.get(LAYOUT_SELECTOR).should('not.exist');
-  });
-});
+    it('displays layout', () => {
+      cy.get(LAYOUT_SELECTOR).should('exist');
+    });
+  }
+);

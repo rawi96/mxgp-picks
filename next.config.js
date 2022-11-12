4200ee4ed401cdf7a28b9abae7d4a97b44a60f97
Raphi <raphael@smartive.ch>
2022-11-12 11:11:31 +0100

    refactor structure

--- next.config.js
+++ next.config.js
@@ -7,4 +7,18 @@ module.exports = withPWA({
     dest: 'public',
     runtimeCaching,
   },
+  async redirects() {
+    return [
+      {
+        source: '/design',
+        destination: 'https://www.figma.com/file/yhoptKQa09IV2KmqKLeyw4/MXGP-PICKS?node-id=0:1',
+        permanent: true,
+      },
+      {
+        source: '/src',
+        destination: 'https://github.com/rawi96/mxgp-picks',
+        permanent: true,
+      },
+    ];
+  },
 });
